4c55d6923c3713a1f840ae4a39d084913a3fbe8b
Fiko <firuz0643@gmail.com>
2025-09-09 10:09:53 +0400

    first slider in head1 section reADY

--- src/js/mian.js
+++ src/js/mian.js
@@ -1,1 +1,16 @@
 import '../sass/style.scss';
+
+
+
+    var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true, // чтобы слайды крутились по кругу
+    autoplay: {
+      delay: 5000, // 5 секунд
+      disableOnInteraction: false, // не останавливать после ручного свайпа
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
